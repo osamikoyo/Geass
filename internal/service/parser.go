437614013501c62bfd7c659392d517435a288e95
osamikoyo <v.pylayev@mail.ru>
2025-02-05 05:43:04 +0500

    add parser

---
+++ internal/service/parser.go
@@ -0,0 +1,89 @@
+package service
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+type visitedURLs struct {
+	mu   sync.Mutex
+	urls map[string]bool
+}
+
+func (v *visitedURLs) add(url string) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	v.urls[url] = true
+}
+
+func (v *visitedURLs) isVisited(url string) bool {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	return v.urls[url]
+}
+
+var visited = visitedURLs{urls: make(map[string]bool)}
+
+func extractLinks(n *html.Node, baseURL string, depth int, wg *sync.WaitGroup) {
+    defer wg.Done()
+
+    if n.Type == html.ElementNode {
+        switch n.Data {
+        case "a":
+            for _, attr := range n.Attr {
+                if attr.Key == "href" {
+                    link := attr.Val
+                    absoluteURL := resolveURL(baseURL, link)
+
+                    if !visited.isVisited(absoluteURL) {
+                        visited.add(absoluteURL)
+                        fmt.Println("Ссылка:", absoluteURL)
+                        if depth < 2 {
+                            wg.Add(1)
+                            go parsePage(absoluteURL, depth+1, wg)
+                        }
+                    }
+                    break
+                }
+            }
+        }
+    }
+
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        wg.Add(1)
+        go extractLinks(c, baseURL, depth, wg)
+    }
+}
+
+func parsePage(url string, depth int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return
+	}
+
+	wg.Add(1)
+	extractLinks(doc, url, depth, wg)
+}
+
+func resolveURL(baseURL, link string) string {
+	u, err := url.Parse(link)
+	if err != nil {
+		return ""
+	}
+	base, err := url.Parse(baseURL)
+	if err != nil {
+		return ""
+	}
+	return base.ResolveReference(u).String()
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -2,10 +2,9 @@ package service
 
 import (
 	"fmt"
-	"net/http"
+	"sync"
 
 	"github.com/osamikoyo/geass/pkg/loger"
-	"golang.org/x/net/html"
 )
 
 type Service struct {
@@ -14,60 +13,14 @@ type Service struct {
 	Contents map[string]string
 }
 
-func (s *Service) AddUrl(url string) {
-	s.URLS = append(s.URLS, url)
-}
-
-func (s *Service) traverse(n *html.Node, url string) {
-	if n.Type == html.ElementNode && n.Data == "li" {
-		var name string
-		var price string
-		var imageURL string
-
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-		switch c.Data {
-			case "h2":
-			if c.FirstChild != nil && c.FirstChild.Type == html.TextNode {
-				name = c.FirstChild.Data
-			}
-			case "span":
-			for _, _ = range c.Attr {
-				if c.FirstChild != nil && c.FirstChild.Type == html.TextNode {
-						price = c.FirstChild.Data
-				}
-			}
-			case "img":
-			for _, a := range c.Attr {
-				if a.Key == "src" {
-					imageURL = a.Val
-				}
-			}
-			}
-		}
-		fmt.Println("Product Name:", name)
-		fmt.Println("Price:", price)
-		fmt.Println("Image URL:", imageURL)
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		s.traverse(c, url)
-	}
-}
 
 func (s *Service) Start(u string) error {
-		s.Logger.Info().Str("URL", u).Msg("Started request")
+	var wg sync.WaitGroup
 
-		resp, err := http.Get(u)
-		if err != nil{
-			s.Logger.Error().Str("URL", u).Msg("Cant do a request: " + err.Error())
-		}
-		defer resp.Body.Close()
+	wg.Add(1)
+	go parsePage(u, 1, &wg)
 
-		doc, err := html.Parse(resp.Body)
-		if err != nil{
-			s.Logger.Error().Str("URL", u).Msg("Cant parse to html: " + err.Error())
-		}
-		s.traverse(doc , u)
-
+	wg.Wait()
 	return nil
 }
 
