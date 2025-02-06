c6b995fb825eae965d9a3c8f2aee43a1c75f2cc9
osamikoyo <v.pylayev@mail.ru>
2025-02-06 06:52:20 +0500

    loger update

--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	golang.org/x/sys v0.29.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/loger/loger.go
+++ pkg/loger/loger.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/rs/zerolog"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 type Logger struct {
@@ -14,7 +15,22 @@ type Logger struct {
 func New() Logger {
 	logger := zerolog.New(
 		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
-	).Level(zerolog.TraceLevel).With().Timestamp().Caller().Logger()
+	).Level(zerolog.TraceLevel)
 
-	return Logger{Logger: &logger}
+
+	fileWriter := &lumberjack.Logger{
+		Filename:   "app.log",
+		MaxSize:    10, // Максимальный размер файла в мегабайтах
+		MaxBackups: 3,  // Максимальное количество старых файлов
+		MaxAge:     28, // Максимальное количество дней хранения
+		Compress:   true, // Сжатие старых файлов
+	}
+
+	fileLogger := zerolog.New(fileWriter)
+
+
+	multi := zerolog.MultiLevelWriter(logger, fileLogger)
+	lg := zerolog.New(multi).With().Timestamp().Caller().Logger()
+
+	return Logger{Logger: &lg}
 }
