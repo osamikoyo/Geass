afd7d1cbaf697fad88e54cd4779d037474114532
osamikoyo <v.pylayev@mail.ru>
2025-02-06 06:42:03 +0500

    condotion of content adding

--- internal/service/get_text_content.go
+++ internal/service/get_text_content.go
@@ -6,20 +6,24 @@ import (
 	"golang.org/x/net/html"
 )
 
-const enter = `
-
-`
-
 func (s *Service) GetTextContent(n *html.Node) (string, error) {
 	var text string
 
 	if n.Type == html.ElementNode {
-		switch n.Data{
-		case "title":
-			if n.FirstChild != nil {
+		if n.Data == "title" {
+			if n.FirstChild != nil{
 				text = fmt.Sprintf("%s%s\n\n", text, n.FirstChild.Data)
 			}
-		
 		}
+		if n.Data == "h1" || n.Data == "h2" || n.Data == "h3"||
+		n.Data == "h4"  || n.Data == "h5" || n.Data == "h6" ||
+		n.Data == "a" || n.Data == "span" || n.Data == "p"{
+			if n.FirstChild != nil {
+				text = fmt.Sprintf("%s%s\n", text, n.FirstChild.Data)
+			}
+		}
+
 	}
+
+	return text,nil
 }
